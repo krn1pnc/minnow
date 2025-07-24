ab2fe39274b41e0fc68ec2f335613f90e31bc7e9
krn1pnc <krn1pnc@outlook.com>
2025-07-24 23:03:06 +0800

    check2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,18 +1,35 @@
 #include "tcp_receiver.hh"
-#include "debug.hh"
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+  if ( message.SYN ) {
+    message.seqno = message.seqno + 1;
+    byte_start_no_ = message.seqno;
+  }
+  if ( message.RST ) {
+    byte_start_no_.reset();
+  }
+  if ( !byte_start_no_.has_value() ) {
+    reassembler_.reader().set_error();
+    return;
+  }
+  reassembler_.insert( message.seqno.unwrap( byte_start_no_.value(), reassembler_.writer().bytes_pushed() ),
+                       message.payload,
+                       message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  debug( "unimplemented send() called" );
-  return {};
+  auto w = reassembler_.writer();
+  TCPReceiverMessage res;
+  if ( w.has_error() ) {
+    res.RST = true;
+  }
+  if ( byte_start_no_.has_value() ) {
+    res.ackno = Wrap32::wrap( w.bytes_pushed() + w.is_closed(), byte_start_no_.value() );
+  }
+  res.window_size = min<uint64_t>( w.available_capacity(), UINT16_MAX );
+  return res;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,19 @@
 #include "wrapping_integers.hh"
-#include "debug.hh"
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32 { static_cast<uint32_t>( zero_point.raw_value_ + n ) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint64_t rem = raw_value_ - zero_point.raw_value_;
+  if ( rem > checkpoint ) {
+    return rem;
+  }
+  uint64_t k = ( checkpoint - rem ) >> 32;
+  uint64_t l = ( k << 32 ) + rem, r = ( ( k + 1 ) << 32 ) + rem;
+  return checkpoint - l < r - checkpoint ? l : r;
 }
